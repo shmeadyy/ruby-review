8c514da955bfdd4c26c5d3016bc027a6dc53cd49
shmeadyy <shmeadyy@github.com>
2021-04-22 10:04:34 -0700

    Added Gemfile and Octokit dependencies

--- action.rb
+++ action.rb
@@ -1,1 +1,11 @@
+require 'octokit'
+
+# Provide authentication credentials
+client = Octokit::Client.new(:access_token => 'personal_access_token')
+
+# Fetch the current user
+user = client.user 'shmeadyy'
+puts user.name
+
+
 puts "Hello, World!"
